94d853cdfc1ba89b80d591735f08c7b10bd5e175
nam-hackintosh <lenkc98@gmail.com>
2018-11-06 08:54:56 +0700

    Minor

--- src/containers/RegisterForm/RegisterForm.js
+++ src/containers/RegisterForm/RegisterForm.js
@@ -4,7 +4,11 @@ import "./registerform.css"
 export default class RegisterForm extends Component {
   render() {
     return(
-      <div></div>
+      <div className="regform">
+        <div className="regform-body">
+          
+        </div>
+      </div>
     )
   }
 }
